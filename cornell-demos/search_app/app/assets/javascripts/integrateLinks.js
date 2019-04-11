7792ed8924c4c8020473bed57be47fbba195db31
hudajkhan <hjk54@cornell.edu>
2019-04-10 20:39:39 -0400

    retrieving URI from LOC for authority

--- cornell-demos/search_app/app/assets/javascripts/integrateLinks.js
+++ cornell-demos/search_app/app/assets/javascripts/integrateLinks.js
@@ -11,11 +11,33 @@ $('*[data-auth]').click(function() {
             url: lookupURL,
             dataType: 'jsonp',
             success: function (data) {
-              console.log("what is the data");console.log(data);
-              return data;
+              urisArray = parseLOCSuggestions(data);
+	      if(urisArray && urisArray.length) {
+		var locURI = urisArray[0]; //Pick first one
+                e.popover({content: 'URI is ' + locURI, html:true, trigger:'focus'}).popover('show');
+              }
             }
 	});
-        e.popover({content: 'http://www.google.com', html:true, trigger:'focus'}).popover('show');
+      
    
 });
+
+//function to process results from LOC lookup
+
+function parseLOCSuggestions(suggestions) {
+var urisArray = [];
+if ( suggestions && suggestions[1] !== undefined ) {
+            for (var s=0; s < suggestions[1].length; s++) {
+                //var l = suggestions[1][s];
+                var u = suggestions[3][s];
+                urisArray.push(u);
+            }
+}
+return urisArray;
+
+
+}
+
+
+
 });
