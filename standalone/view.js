fd03db1440534cc0c7235ffe212150e5ade45a31
hjk54 <hjk54@cornell.edu>
2022-01-28 13:35:03 -0500

    view for fuseki endpoint

---
+++ standalone/view.js
@@ -0,0 +1,11 @@
+class View {
+	
+	  constructor(fusekiURL) {
+	  	this.fusekiURL = fusekiURL;
+	  }
+}
+
+$( document ).ready(function() {
+    var fusekiURL = "http://deep-thought.slis.uiowa.edu:3030/"
+	var viewObj = new View();
+}
