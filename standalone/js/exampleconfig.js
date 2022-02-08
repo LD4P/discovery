7791a465f34df07317d11fd09f7f72c311948844
U-CORNELL\hjk54 <hjk54@LIB-HJK54-lap.library.cornell.edu>
2022-02-07 20:20:11 -0500

    restructuring folders

---
+++ standalone/js/exampleconfig.js
@@ -0,0 +1,7 @@
+var configProps = {
+	//"fusekiURL": "http://[URL]/[dataset]/sparql"
+};
+
+//Directions: Please rename this file to config.js.  
+//Uncomment the line in configProps above and update the value to point to the
+//SPARQL endpoint you wish to employ.
